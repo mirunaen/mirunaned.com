55a0e9a7d67fb16f78a309fbb3bef0f805df155d
mirunaen <mirunaen@gmail.com>
2020-05-19 21:09:33 +0200

    Changed profile pic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Super Cool Personal Blog`,
     author: {
       name: `Miruna Nedelcu`,
       summary: `who enjoys life.`,
@@ -8,7 +8,7 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `miruna85740032`,
     },
   },
   plugins: [
