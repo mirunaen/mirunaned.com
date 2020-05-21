93f280affec6a298a7d4d692dbe9bf081a788e06
mirunaen <mirunaen@gmail.com>
2020-05-21 20:13:52 +0200

    css

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,6 @@
 import React from "react"
 //import Header from "../components/header"
 import { Link } from "gatsby"
-import Layout from "../components/layout"
 
 export default function About() {
   return (
