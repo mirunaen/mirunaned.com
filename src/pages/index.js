92dcdb8ab3fa57bb43e50fa26a069a92b19092ab
mirunaen <mirunaen@gmail.com>
2020-05-22 19:22:28 +0200

    scroll animation library

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+import Image from "gatsby-image"
+
+export default function Contact() {
+  var sal = require("sal.js")
+  return (
+    <h1 class="header--text">
+      A place for{" "}
+      <span id="typed" class="typed">
+        mu
+      </span>
+      <span class="typed-cursor">|</span>
+    </h1>
+  )
+}
